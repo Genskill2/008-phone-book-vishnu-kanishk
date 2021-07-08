3d6a7fa4cde2c9556119890180a9f64ae869e311
Vishnu Kanishk <68165401+vishnu-kanishk@users.noreply.github.com>
2021-07-08 16:03:25 +0530

    Update phone_book.c

--- phone_book.c
+++ phone_book.c
@@ -21,17 +21,17 @@ int delete(FILE *, char *);
 /* Utility functions  */
 FILE * open_db_file(); /* Opens the database file. Prints error and
                           quits if it's not available */
-void print_usage(char , char *);  / Prints usage */
-entry* load_entries(FILE );         / Load all entries from the
+void print_usage(char , char *);  /* Prints usage */
+entry* load_entries(FILE );         /* Load all entries from the
                                       database file. Returns pointer
                                       to first entry */
-entry* create_entry_node(char , char *);  / Create a new entry
+entry* create_entry_node(char , char *);  /* Create a new entry
                                               node. Has to be freed by
                                               user. */
-void free_entries(entry ); / TBD Given the first node of a linked list
+void free_entries(entry ); /* TBD Given the first node of a linked list
                                of entries, will free all the nodes */ 
 
-void write_all_entries(entry ); / Given the first node of a linked
+void write_all_entries(entry ); /* Given the first node of a linked
                                     list of entries, will delete the
                                     database file on the disk and save
                                     the given entries into the file */
